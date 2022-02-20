54a7f35c38270cb14f4e5d397fad047c2b3fd663
rogoznik <nikrogoz@gmail.com>
2022-02-20 15:56:17 +0500

    fix

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -6,6 +6,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = models.CharField(max_length=64, unique=True)
-    firstname = models.CharField(max_length=64)
-    lastname = models.CharField(max_length=64)
     email = models.EmailField(unique=True)
